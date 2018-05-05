6f191c88162b4e66725f8c3927d37cd2f34d57eb
Jan Willhaus <mail@janwillhaus.de>
2018-05-05 16:11:07 +0200

    Cleanup refresh command

--- podcasts/management/commands/refresh_feeds.py
+++ podcasts/management/commands/refresh_feeds.py
@@ -1,18 +1,13 @@
 from django.apps import apps
 from django.core.management.base import BaseCommand
 
-from podcasts.utils import refresh_feed
-
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = "My shiny new management command."
-
-    # def add_arguments(self, parser):
-        # parser.add_argument('--verbosity', type=int)
+    help = "Refresh feeds of all podcasts in the database."
 
     def handle(self, *args, **options):
         verbosity = int(options['verbosity'])
@@ -21,14 +16,7 @@ class Command(BaseCommand):
             root_logger.setLevel(logging.DEBUG)
 
         Podcast = apps.get_model('podcasts', 'Podcast')
-
         for podcast in Podcast.objects.iterator():
-            logger.info('Refreshing feed: %s ...' % podcast.title)
-
-            info = refresh_feed(podcast.feed_url)
-            logger.debug('Received info', info)
-
-
-            podcast.create_episodes(info)
-
-        # raise NotImplementedError()
+            logger.info("Updating podcast feed: %s ..." % podcast.title)
+            podcast.update()
+            podcast.save()
