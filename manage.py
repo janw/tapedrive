041d5d7b9070fadbfc73a0297c674931358f44d6
Jan Willhaus <mail@janwillhaus.de>
2018-05-06 13:36:07 +0200

    Allow clean fallback to Py2 with missing VirtualEnv

--- manage.py
+++ manage.py
@@ -13,5 +13,5 @@ if __name__ == '__main__':
         raise ImportError(
             'Couldn\'t import Django. Are you sure it\'s installed and '
             'available on your PYTHONPATH environment variable? Did you '
-            'forget to activate a virtual environment?') from exc
+            'forget to activate a virtual environment?')
     execute_from_command_line(sys.argv)
