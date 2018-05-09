756a1de27f4ad32607bedc258573dd5deb1279ec
Jan Willhaus <mail@janwillhaus.de>
2018-05-09 18:24:31 +0200

    Subscribe/unsubscribe API mechanism

--- podcastarchive/settings.py
+++ podcastarchive/settings.py
@@ -125,7 +125,7 @@ class Common(Configuration):
     # https://docs.djangoproject.com/en/2.0/topics/i18n/
     LANGUAGE_CODE = 'en-us'
 
-    TIME_ZONE = 'UTC'
+    TIME_ZONE = 'Europe/Berlin'
 
     USE_I18N = True
 
--- podcasts/urls.py
+++ podcasts/urls.py
@@ -13,9 +13,11 @@ episodes_patterns = [
     ]))
 ]
 
-api_patterns = [
-    path('refresh-feed/<slug:slug>/', api.podcasts_refresh_feed, name='api-refresh-feed'),
-    path('episodes/', include(episodes_patterns)),
+podcasts_api_patterns = [
+    path('refresh-feed/', api.podcast_refresh_feed, name='api-refresh-feed'),
+    path('subscribe/', api.podcast_subscribe, name='api-subscribe'),
+    path('unsubscribe/', api.podcast_unsubscribe, name='api-unsubscribe'),
+    # path('episodes/', include(episodes_patterns)),
 ]
 
 
@@ -25,13 +27,13 @@ podcasts_patterns = [
     path('new/', views.podcasts_new, name='podcasts-new'),
     path('<slug:slug>/', include([
         path('', views.podcasts_details, name='podcasts-details'),
+        path('api/', include(podcasts_api_patterns)),
         path('refresh/', views.podcasts_refresh_feed, name='podcasts-refresh-feed'),
     ]))
 ]
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('api/', include(api_patterns)),
     path('podcasts/', include(podcasts_patterns)),
     path('settings/', views.user_settings, name='podcasts-user-settings'),
 ]
