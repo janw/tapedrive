dd331a9ba72eb0a248c24ff79c233c7495e958b1
Jan Willhaus <mail@janwillhaus.de>
2018-05-12 15:25:37 +0200

    Minor in-between fixes

--- podcastarchive/wsgi.py
+++ podcastarchive/wsgi.py
@@ -11,6 +11,6 @@ configuration = os.getenv('ENVIRONMENT', 'development').title()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'podcastarchive.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', configuration)
 
-from configurations.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application # noqa
 
 application = get_wsgi_application()
