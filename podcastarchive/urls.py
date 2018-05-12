9a99bf5577aa40830dc10cd19a48fe94758e6d57
Jan Willhaus <mail@janwillhaus.de>
2018-05-12 15:12:36 +0200

    Collection of registration templates, pass reset/change

--- podcastarchive/urls.py
+++ podcastarchive/urls.py
@@ -5,11 +5,14 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 password_reset_patterns = [
-    path('', auth_views.password_reset, name='password_reset'),
-    path('requested/', auth_views.password_reset_done, name='password_reset_done'),
-    re_path(r't/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    path('change/', auth_views.password_change, name='password_change'),
+    path('change/done/', auth_views.password_change_done, name='password_change_done'),
+
+    path('reset/', auth_views.password_reset, name='password_reset'),
+    path('reset/requested/', auth_views.password_reset_done, name='password_reset_done'),
+    re_path(r'reset/t/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
             auth_views.password_reset_confirm, name='password_reset_confirm'),
-    path('done/', auth_views.password_reset_complete, name='password_reset_complete'),
+    path('reset/done/', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
 
 urlpatterns = [
@@ -20,7 +23,7 @@ urlpatterns = [
     path('login/', auth_views.login, name='login'),
     path('logout/', auth_views.logout, name='logout'),
 
-    path('password-reset/', include(password_reset_patterns)),
+    path('password/', include(password_reset_patterns)),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
