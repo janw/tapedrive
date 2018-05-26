890225f0957a6f62c2bffedba294c85530096256
Jan Willhaus <mail@janwillhaus.de>
2018-05-26 16:37:58 +0200

    Update default storage and naming scheme for better OOTB exp.

--- podcasts/conf.py
+++ podcasts/conf.py
@@ -19,8 +19,8 @@ DEFAULT_EPISODES_ORDER = _getattr('EPISODES_ORDER', '-published')
 SEEK_FORWARD_BY = _getattr('SEEK_FORWARD_BY', 45)
 SEEK_BACKWARD_BY = _getattr('SEEK_BACKWARD_BY', 30)
 
-STORAGE_DIRECTORY = '$HOME/'
-DEFAULT_NAMING_SCHEME = '$podcast_slug/$episode_title'
+STORAGE_DIRECTORY = '/data'
+DEFAULT_NAMING_SCHEME = '$podcast_slug/$episode_slug'
 DEFAULT_DATE_FORMAT = 'Y-m-d_Hi'
 
 ITUNES_TOPCHARTS_URL = 'https://rss.itunes.apple.com/api/v1/us/podcasts/top-podcasts/all/25/explicit.json'
