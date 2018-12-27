5071f74164c40defa697f28e884a362a6752e662
Jan Willhaus <mail@janwillhaus.de>
2018-12-27 20:58:12 +0100

    Adjust prepare script for webpack

--- prepare.sh
+++ prepare.sh
@@ -6,5 +6,5 @@ echo "Compiling messages."
 python manage.py compilemessages
 
 echo "Compiling and collecting static files"
-gulp build
+webpack --mode production
 python manage.py collectstatic --no-input -v0 --ignore "src"
