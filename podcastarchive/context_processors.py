637bc90559e691bf1332ec9da50a4f3c5c3ce101
Jan Willhaus <mail@janwillhaus.de>
2019-05-04 15:10:33 +0200

    Make Black ignore autogenerated migrations

--- podcastarchive/context_processors.py
+++ podcastarchive/context_processors.py
@@ -1,4 +1,3 @@
-
 from django.conf import settings
 
 
@@ -7,6 +6,6 @@ def debug(request):
     Return additional context variables helpful for debugging parts of the project.
     """
     context_extras = {}
-    if request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS:
-        context_extras['DEBUG_FRONTEND'] = getattr(settings, 'DEBUG_FRONTEND', False)
+    if request.META.get("REMOTE_ADDR") in settings.INTERNAL_IPS:
+        context_extras["DEBUG_FRONTEND"] = getattr(settings, "DEBUG_FRONTEND", False)
     return context_extras
