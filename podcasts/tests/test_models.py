cb03930bb8f97dd7ac0f119012a18c2fe1f3ef76
Jan Willhaus <mail@janwillhaus.de>
2019-05-07 20:06:22 +0200

    Migrate podcasts models tests to use VCRpy

--- podcasts/tests/test_models.py
+++ podcasts/tests/test_models.py
@@ -1,3 +1,7 @@
+import logging
+
+import pytest
+
 from django.test import TestCase
 
 from django.contrib.sites.models import Site
@@ -8,12 +12,12 @@ from podcasts.models.podcast import Podcast
 from podcasts.models import PodcastsSettings
 
 from datetime import datetime, timezone
+from podcasts.tests.test_utils import TEST_FEED_NEXT_PAGE
 
 # Create your tests here.
 
 TEST_FEED = "http://feeds.5by5.tv/killingtime"
-TEST_FEED_PAGED = "http://cre.fm/feed/m4a"
-TEST_FEED_PAGED_SECOND_PAGE = "?paged=2"
+TEST_FEED_PAGED = TEST_FEED_NEXT_PAGE
 
 TEST_PODCAST = {
     "feed_url": "http://example.com/feed",
@@ -28,151 +32,120 @@ TEST_EPISODE = {
 }
 
 
-class UtilFunctionsTestCase(TestCase):
-    pass
+@pytest.mark.django_db
+@pytest.mark.vcr()
+def test_podcast_model(caplog):
+    """Test creation of Podcast model from different inputs"""
+    Podcast.objects.create(feed_url=TEST_FEED)
+
+    podcast = Podcast.objects.get(feed_url=TEST_FEED)
+    assert podcast.title == ""
+
+    with pytest.raises(IntegrityError):
+        Podcast.objects.create_from_feed_url(TEST_FEED)
+
+    with caplog.at_level(logging.INFO, logger="podcasts.models"):
+        podcast.update()
+
+    assert "Fetched Killing Time" in caplog.text
+    assert podcast.title == "Killing Time"
+    assert podcast.get_absolute_url() == "/podcasts/killing-time/"
+
+    # get_or_create should return the same podcast now
+    same_one, created = Podcast.objects.get_or_create_from_feed_url(TEST_FEED)
+    assert podcast == same_one
+    assert created is False
+
+    # Should have more than 0 episodes
+    assert Episode.objects.count() > 0
+
+    # Deletion should remove Podcast and cascade to Episodes
+    podcast.delete()
+    Podcast.objects.count() == 0
+    Episode.objects.count() == 0
 
 
-class PodcastModelTestCase(TestCase):
-    def setUp(self):
-        Podcast.objects.create(feed_url=TEST_FEED)
+@pytest.mark.django_db
+@pytest.mark.vcr()
+def test_podcast_with_paged_feed(caplog):
+    """Create complete Podcast from feed_url, with pages."""
 
-    def test_podcast_model(self):
-        """Test creation of Podcast model from different inputs"""
-        podcast = Podcast.objects.get(feed_url=TEST_FEED)
-        self.assertEqual(podcast.title, "")
-
-        with self.assertRaises(IntegrityError):
-            Podcast.objects.create_from_feed_url(TEST_FEED)
-
-        with self.assertLogs("podcasts.models", level="INFO") as logs:
-            podcast.update()
-            self.assertEqual(podcast.title, "Killing Time")
-
-        self.assertIn(
-            "INFO:podcasts.models.podcast:Fetched Killing Time", logs.output[0]
+    with caplog.at_level(logging.INFO, logger="podcasts.models"):
+        podcast, created = Podcast.objects.get_or_create_from_feed_url(
+            TEST_FEED_NEXT_PAGE
         )
-        self.assertEqual(
-            logs.output[1:],
-            [
-                "INFO:podcasts.models.podcast:Inserting cover image",
-                "INFO:podcasts.models.podcast:Inserting episodes from first page",
-                "INFO:podcasts.models.podcast:No next page found",
-                "INFO:podcasts.models.podcast:All done",
-            ],
-        )
+    assert created
+    assert isinstance(podcast, Podcast)
 
-        self.assertEqual(podcast.get_absolute_url(), "/podcasts/killing-time/")
+    assert "Creating CRE" in caplog.text
+    assert "No next page found" in caplog.text
+    caplog.clear()
 
-        # get_or_create should return the same podcast now
-        same_one, created = Podcast.objects.get_or_create_from_feed_url(TEST_FEED)
-        self.assertEqual(podcast, same_one)
-        self.assertFalse(created)
+    # Update Podcast, detection of existing episodes
+    with caplog.at_level(logging.INFO, logger="podcasts.models"):
+        podcast.update(insert_cover=False)
 
-        # Should have more than 0 episodes
-        self.assertGreaterEqual(Episode.objects.count(), 0)
-
-        # Deletion should remove Podcast and cascade to Episodes
-        podcast.delete()
-        self.assertEqual(Podcast.objects.count(), 0)
-        self.assertEqual(Episode.objects.count(), 0)
-
-        # Create complete Podcast from feed_url, with pages
-        with self.assertLogs("podcasts.models", level="INFO") as logs:
-            podcast, created = Podcast.objects.get_or_create_from_feed_url(
-                TEST_FEED_PAGED
-            )
-            self.assertTrue(created)
-            self.assertTrue(isinstance(podcast, Podcast))
-
-        self.assertIn("INFO:podcasts.models.podcast:Creating CRE", logs.output[0])
-        self.assertEqual(
-            logs.output[-2:],
-            [
-                "INFO:podcasts.models.podcast:No next page found",
-                "INFO:podcasts.models.podcast:All done",
-            ],
-        )
-
-        # Update Podcast, detection of existing episodes
-        with self.assertLogs("podcasts.models", level="INFO") as logs:
-            podcast.update(insert_cover=False)
-
-        self.assertEqual(
-            logs.output[1:],
-            [
-                "INFO:podcasts.models.podcast:Inserting episodes from first page",
-                "INFO:podcasts.models.podcast:Found existing episodes",
-                "INFO:podcasts.models.podcast:All done",
-            ],
-        )
+    assert "Inserting episodes from first page" in caplog.text
+    assert "Found existing episodes" in caplog.text
 
 
-class FilenameCreationTestCase(TestCase):
+@pytest.mark.django_db
+def test_filename_generation():
     valid_naming_scheme = "$podcast_type/$podcast_slug/${episode_date}_$episode_title"
     invalid_naming_scheme = "$podcast_type/${podcast_slug}_$episode_testattr"
     datefmt = "Y-m-d_Hi"
 
-    def setUp(self):
-        self.podcast = Podcast.objects.create(**TEST_PODCAST)
-        self.episode = self.podcast.episodes.create(**TEST_EPISODE)
+    podcast = Podcast.objects.create(**TEST_PODCAST)
+    episode = podcast.episodes.create(**TEST_EPISODE)
 
-    def test_valid_call(self):
-        """Check everything's fine with a valid naming scheme"""
-        should_be = "serial/test-feed/2018-03-12_1000_Le fancey episode.mp3"
+    should_be = "serial/test-feed/2018-03-12_1000_Le fancey episode.mp3"
+    file_path = episode.construct_file_path("", valid_naming_scheme, datefmt)
+    assert file_path == should_be
 
-        file_path = self.episode.construct_file_path(
-            "", self.valid_naming_scheme, self.datefmt
+    # Check that an invalid segments is untouched
+    should_be = "serial/test-feed_$episode_testattr.mp3"
+    file_path = episode.construct_file_path("", invalid_naming_scheme, datefmt)
+    assert file_path == should_be
+
+
+@pytest.mark.django_db
+def test_settings_model():
+    site = Site.objects.create(name="Test Site", domain="testsite.local")
+
+    # Creating a Site object, fires a post_save creation of PodcastsSettings
+    # therefore trying to create another PS object with the same Site should
+    # cause the unique constraint to kick in
+    with pytest.raises(IntegrityError):
+        PodcastsSettings.objects.create(site=site)
+
+    # OneToOne rel should contain the PS instance
+    settings = site.podcastssettings
+    assert isinstance(settings, PodcastsSettings)
+
+    # Return the proper __str__ based on the Site.name
+    assert str(settings) == "Test Site Podcasts Settings"
+
+
+@pytest.mark.django_db
+@pytest.mark.vcr()
+def test_episode_model():
+    podcast = Podcast.objects.create(feed_url=TEST_FEED)
+    podcast.update()
+
+    # Episode object cannot be created empty (requires GUID and Podcast)
+    with transaction.atomic():
+        with pytest.raises(IntegrityError):
+            episode = Episode.objects.create()
+    with transaction.atomic():
+        with pytest.raises(IntegrityError):
+            episode = Episode.objects.create(guid="testguid")
+
+    with transaction.atomic():
+        episode = Episode.objects.create(guid="/testguid1/", podcast=podcast)
+    assert str(episode) == "Killing Time's Episode"
+
+    with transaction.atomic():
+        episode = Episode.objects.create(
+            guid="/testguid2/", podcast=podcast, title="Fancy Test Episode"
         )
-        self.assertEqual(file_path, should_be)
-
-    def test_invalid_segment(self):
-        """Check that an invalid segments is untouched"""
-        should_be = "serial/test-feed_$episode_testattr.mp3"
-        file_path = self.episode.construct_file_path(
-            "", self.invalid_naming_scheme, self.datefmt
-        )
-        self.assertEqual(file_path, should_be)
-
-
-class PodcastsSettingsModelTestCase(TestCase):
-    def test_settings_model(self):
-        site = Site.objects.create(name="Test Site", domain="testsite.local")
-
-        # Creating a Site object, fires a post_save creation of PodcastsSettings
-        # therefore trying to create another PS object with the same Site should
-        # cause the unique constraint to kick in
-        with self.assertRaises(IntegrityError):
-            PodcastsSettings.objects.create(site=site)
-
-        # OneToOne rel should contain the PS instance
-        settings = site.podcastssettings
-        self.assertIsInstance(settings, PodcastsSettings)
-
-        # Return the proper __str__ based on the Site.name
-        self.assertEqual(str(settings), "Test Site Podcasts Settings")
-
-
-class EpisodeModelTestCase(TestCase):
-    def setUp(self):
-        self.podcast = Podcast.objects.create(feed_url=TEST_FEED)
-        self.podcast.update()
-
-    def test_episode_model(self):
-
-        # Episode object cannot be created empty (requires GUID and Podcast)
-        with transaction.atomic():
-            with self.assertRaises(IntegrityError):
-                episode = Episode.objects.create()
-        with transaction.atomic():
-            with self.assertRaises(IntegrityError):
-                episode = Episode.objects.create(guid="testguid")
-
-        with transaction.atomic():
-            episode = Episode.objects.create(guid="/testguid1/", podcast=self.podcast)
-        self.assertEqual(str(episode), "Killing Time's Episode")
-
-        with transaction.atomic():
-            episode = Episode.objects.create(
-                guid="/testguid2/", podcast=self.podcast, title="Fancy Test Episode"
-            )
-        self.assertEqual(str(episode), "Fancy Test Episode")
+    str(episode) == "Fancy Test Episode"
