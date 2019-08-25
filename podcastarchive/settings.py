f8052ec4fbad7becdb1e6344212d212781d930bf
Jan Willhaus <mail@janwillhaus.de>
2019-08-25 14:18:30 +0200

    Remove all django templates, related settings ctx-procs

--- podcastarchive/settings.py
+++ podcastarchive/settings.py
@@ -7,7 +7,6 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-from django.utils.translation import gettext_lazy as _
 
 import os
 import random
@@ -42,25 +41,6 @@ def get_secret_key(PROJECT_DIR):
     return SECRET_KEY
 
 
-class DatabaseURLValueWithOptions(values.DatabaseURLValue):
-    options_mysql = {"charset": "utf8mb4"}
-    options_sqlite = {"timeout": 20}
-
-    def to_python(self, value):
-        value = super().to_python(value)
-        if (
-            value[self.alias]["ENGINE"] == "django.db.backends.mysql"
-            and self.options_mysql is not None
-        ):
-            value[self.alias].update({"OPTIONS": self.options_mysql})
-        if (
-            value[self.alias]["ENGINE"] == "django.db.backends.sqlite"
-            and self.options_sqlite is not None
-        ):
-            value[self.alias].update({"OPTIONS": self.options_sqlite})
-        return value
-
-
 class Common(Configuration):
     # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
     BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -70,21 +50,18 @@ class Common(Configuration):
 
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = values.BooleanValue(False)
-    DEBUG_FRONTEND = values.BooleanValue(False)
 
     ALLOWED_HOSTS = values.ListValue([], environ=True)
 
     # Application definition
     INSTALLED_APPS = [
+        "whitenoise.runserver_nostatic",
         "django.contrib.admin",
         "django.contrib.auth",
         "django.contrib.contenttypes",
         "django.contrib.sessions",
-        "django.contrib.messages",
-        "django.contrib.humanize",
         "django.contrib.sites",
         "django.contrib.staticfiles",
-        "bootstrap4",
         "podcastarchive.users",
         "podcasts",
         "background_task",
@@ -96,7 +73,7 @@ class Common(Configuration):
         "django.middleware.security.SecurityMiddleware",
         "django.contrib.sessions.middleware.SessionMiddleware",
         "django.middleware.common.CommonMiddleware",
-        # "django.middleware.csrf.CsrfViewMiddleware",
+        "whitenoise.middleware.WhiteNoiseMiddleware",
         "django.middleware.locale.LocaleMiddleware",
         "django.contrib.auth.middleware.AuthenticationMiddleware",
         "django.contrib.messages.middleware.MessageMiddleware",
@@ -109,15 +86,12 @@ class Common(Configuration):
     TEMPLATES = [
         {
             "BACKEND": "django.template.backends.django.DjangoTemplates",
-            "DIRS": [os.path.join(BASE_DIR, "templates")],
+            "DIRS": [os.path.join(BASE_DIR, "frontend", "dist")],
             "APP_DIRS": True,
             "OPTIONS": {
                 "context_processors": [
-                    "django.template.context_processors.debug",
                     "django.template.context_processors.request",
                     "django.contrib.auth.context_processors.auth",
-                    "django.contrib.messages.context_processors.messages",
-                    "podcastarchive.context_processors.debug",
                 ]
             },
         }
@@ -127,7 +101,7 @@ class Common(Configuration):
 
     # Database
     # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-    DATABASES = DatabaseURLValueWithOptions(
+    DATABASES = values.DatabaseURLValue(
         "postgres://tapedrive:tapedrive@localhost/tapedrive"
     )
 
@@ -150,23 +124,13 @@ class Common(Configuration):
     # Internationalization
     # https://docs.djangoproject.com/en/2.0/topics/i18n/
     LANGUAGE_CODE = "en-us"
-
     TIME_ZONE = "Europe/Berlin"
-
-    USE_I18N = True
-
-    USE_L10N = True
-
     USE_TZ = True
-
-    LANGUAGES = [("de", _("German")), ("en", _("English"))]
-
-    LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/2.0/howto/static-files/
     STATIC_URL = "/static/"
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATIC_ROOT = os.path.join(BASE_DIR, "frontend", "dist")
     STATICFILES_FINDERS = [
         "django.contrib.staticfiles.finders.FileSystemFinder",
         "django.contrib.staticfiles.finders.AppDirectoriesFinder",
@@ -174,22 +138,12 @@ class Common(Configuration):
     STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
 
     MEDIA_URL = "/media/"
-    MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
     AUTH_USER_MODEL = "users.User"
 
-    DEBUG_TOOLBAR_CONFIG = {"JQUERY_URL": ""}
-
     # Project settings
     COVER_IMAGE_SIZE = (500, 500)
-
-    MESSAGE_TAGS = {
-        10: "alert-dark",
-        20: "alert-info",
-        25: "alert-success",
-        30: "alert-warning",
-        40: "alert-danger",
-    }
 
     ACTSTREAM_SETTINGS = {
         "FETCH_RELATIONS": True,
@@ -222,16 +176,11 @@ class Development(Common):
     """
 
     DEBUG = True
-    DEBUG_FRONTEND = True
 
     INTERNAL_IPS = ["127.0.0.1"]
 
     INSTALLED_APPS = (
         ["livereload"] + Common.INSTALLED_APPS + ["django_extensions", "debug_toolbar"]
-    )
-
-    DATABASES = DatabaseURLValueWithOptions(
-        "sqlite:///{}".format(os.path.join(Common.BASE_DIR, "db.sqlite3"))
     )
 
     MIDDLEWARE = Common.MIDDLEWARE + [
@@ -261,7 +210,7 @@ class Development(Common):
 class Testing(Common):
 
     with TemporaryDirectory() as tmpdirname:
-        DATABASES = DatabaseURLValueWithOptions(
+        DATABASES = values.DatabaseURLValue(
             "sqlite:///{}".format(os.path.join(tmpdirname, "tapedrive-testing.sqlite3"))
         )
 
