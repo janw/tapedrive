96465b9162763367e4618f6957950ec37feeba6f
Jan Willhaus <mail@janwillhaus.de>
2020-01-04 12:14:20 +0100

    Update Dockerfile / build

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-set -e
+set -ex
 
+# Collect static files from external apps
+python manage.py collectstatic --no-input
+
+# Check for database and migrate it
 python manage.py dbconnection
 python manage.py migrate
+
+# Initialize admin account
 python manage.py initadmin
 
 echo "Starting main process."
