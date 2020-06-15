9dfcb62de657be450c4e6e24548e8b4caafecd52
Jan Willhaus <mail@janwillhaus.de>
2020-06-15 10:33:45 +0200

    Remove duplicate logging config

--- tapedrive/settings.py
+++ tapedrive/settings.py
@@ -188,16 +188,6 @@ class Development(Common):
 
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-    LOGGING = {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "handlers": {"console": {"class": "logging.StreamHandler"}},
-        "loggers": {
-            "django": {"handlers": ["console"], "level": "INFO"},
-            "podcasts": {"handlers": ["console"], "level": "INFO"},
-        },
-    }
-
 
 class Testing(Common):
     DATABASES = values.DatabaseURLValue("sqlite:///tapedrive-testing.sqlite3")
